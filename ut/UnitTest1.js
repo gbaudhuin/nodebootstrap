7fc200e09a7228bdcd8d2cbb80ff05de58f3f4ec
Guillaume Baudhuin <g.baudhuin@peoleo.fr>
2016-11-14 09:36:32 +0100

    1st version

---
+++ ut/UnitTest1.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+var assert = require('assert');
+
+describe('Test Suite 1', function() {
+    it('Test 1', function() {
+        assert.ok(true, "This shouldn't fail");
+    });
+
+    it('Test 2', function() {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail");
+    });
+});
